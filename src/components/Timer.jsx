3927dc008d4cef44ce1fa4f7b34574fab72fdf9a
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-04 15:12:15 -0400

    add timer logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,26 +1,57 @@
 import { useState } from 'react';
 
 export default function Timer() {
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0);
+    const [time, setTime] = useState({hours:0, minutes:0, seconds:0});
     const [isRunning, setIsRunning] = useState(false);
 
-    const handleTimerRunning = (event) => {
-        if (!isRunning) {
+    var updatedHours = time.hours,
+        updatedMinutes = time.minutes,
+        updatedSeconds = time.seconds;
+
+    const handleTimer = (event) => {
+        setIsRunning(!isRunning);
+        const updatedIsRunning = !isRunning;
+        console.log(updatedIsRunning);
+        if (updatedIsRunning) {
             event.target.textContent = "Click to Stop";
-            setIsRunning(!isRunning);
+            startTimer();
         } else {
             event.target.textContent = "Click to Start";
             setIsRunning(!isRunning);
         }
     };
 
+    const startTimer = () => {
+        countTime();
+        setInterval(countTime, 1000);
+    };
+
+    const countTime = () => {
+        if (updatedMinutes === 60) {
+            updatedHours++;
+            updatedMinutes = 0;
+        }
+
+        if (updatedSeconds === 60) {
+            updatedMinutes++;
+            updatedSeconds = 0;
+        }
+
+        updatedSeconds++;
+        return setTime({
+            hours: updatedHours,
+            minutes: updatedMinutes,
+            seconds: updatedSeconds,
+        });
+    };
+
     return (
         <>
+            <p>
+                {time.seconds < 10 ? `0${time.seconds}` : time.seconds}
+            </p>
             <button
-                onClick={handleTimerRunning}
-                onSubmit={(e) => e.preventDefault()}
+                onClick={handleTimer}
             >
             Click to Start
             </button>
