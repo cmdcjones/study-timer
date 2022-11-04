6a0954a98a226c23c1de48e7509038f483100b9e
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-04 13:24:04 -0400

    add state

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 
 export default function Timer() {
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+
     return (
         <>
             <h1>Hello world!</h1>
