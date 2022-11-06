9375edb8f7fabaf9ac9560381b5d7b56202750b0
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-06 12:18:28 -0500

    move start button logic to StartButton

---
+++ src/components/StartButton.jsx
@@ -0,0 +1,55 @@
+
+export default function StartButton({
+    time,
+    setTime,
+    setIntervalId,
+    buttonStatus,
+    setButtonStatus,
+    setIsRunning,
+}) {
+    let updatedHours = time.hours,
+        updatedMinutes = time.minutes,
+        updatedSeconds = time.seconds,
+        updatedMilliseconds = time.milliseconds;
+
+    const startTimer = () => {
+        setButtonStatus('Pause');
+        setIsRunning(1);
+        countTime();
+        setIntervalId(setInterval(countTime, 10));
+    };
+
+    const countTime = () => {
+        if (updatedMinutes > 59) {
+            updatedHours++;
+            updatedMinutes = 0;
+        }
+
+        if (updatedSeconds > 59) {
+            updatedMinutes++;
+            updatedSeconds = 0;
+        }
+        if (updatedMilliseconds > 99) {
+            updatedSeconds++;
+            updatedMilliseconds = 0;
+        }
+        updatedMilliseconds++;
+
+        return setTime({
+            hours: updatedHours,
+            minutes: updatedMinutes,
+            seconds: updatedSeconds,
+            milliseconds: updatedMilliseconds,
+        });
+    };
+
+    return (
+        <>
+            <button 
+                onClick={startTimer}
+            >
+            {buttonStatus}
+            </button>
+        </>
+    );
+}
